f2658d3e36b65e1a5f53daaa5e4b7077c41684f9
JamalBoustani <boustani11@gmail.com>
2022-12-07 17:29:02 +0000

    implementing frequencies function to frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,9 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for i in items:
+        if frequencies.get(str(i)) == None:
+            frequencies[str(i)]= 1 
+        else:
+            frequencies[str(i)]+= 1
     return frequencies
